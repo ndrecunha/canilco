e80171cde65c4b1911905cc930277b4e4c88ee9d
Andre Cunha <accunha@gmail.com>
2013-12-27 15:10:04 -0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Canilco::Application.config.secret_key_base = 'f98a7e360d469e43c53b16666e3863f6373ea151f1e00591938dc1102a96975dcbf058bfe2a409f51421f6a6c344ae53cbb56668ea168c6885a54e493b4932c0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Canilco::Application.config.session_store :cookie_store, key: '_canilco_session'
