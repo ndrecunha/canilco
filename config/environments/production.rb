50636c304d2b76a11fb42839eaf90d5ea3170fb4
Andre Cunha <accunha@gmail.com>
2014-01-02 17:33:28 -0200

    Implemented Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Canilco::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+    #required for devise
+  config.action_mailer.default_url_options = { :host => 'http://canilco.herokuapp.com/' } 
+
 end
